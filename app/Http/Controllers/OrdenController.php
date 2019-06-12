9644ac491dc1454ff2af1aaabd656650a8faec48
Encarnacion Cituk Caamal <programaciondyn3.mx@sandos.com>
2019-06-12 14:49:32 -0500

    Se muestran categorias
    This reverts commit 9f248bfcd9c7ccb997c956bd5172f70250a281a8.

--- app/Http/Controllers/OrdenController.php
+++ app/Http/Controllers/OrdenController.php
@@ -17,10 +17,9 @@ class OrdenController extends Controller
         // $respuesta = json_decode($respuesta);
         $zonas = $respuesta->objeto;
         $alergenos = \App::call('App\Http\Controllers\AlergenoController@obtenerTodosLosAlergenos');
-        $categorias = \App::call( 'App\Http\Controllers\CategoriaController@obtenerTodasLasCategorias');
                 
         // dd($zonas);
-        return view('ordenar', compact('zonas','alergenos','categorias'));
+        return view('ordenar', compact('zonas','alergenos'));
     }
    
     public function obtenerTodasLasZonasPV($idPuntoVenta){
--- resources/views/ordenar/partials/modalOpenMesa.blade.php
+++ resources/views/ordenar/partials/modalOpenMesa.blade.php
@@ -18,9 +18,9 @@
                         <span class="input-group-addon">
                             <i class="fas fa-hotel"></i>
                         </span>
-                        <div class="form-group label-floating">                            
+                        <div class="form-group label-floating">
+                            <label class="control-label">Codigo Hotel</label>
                             <input id="codigoHotel" type="text" class="form-control" name="codigoHotel" value="CARACOL">  
-                            <input id="idMesa" type="text" class="form-control" name="idMesa">  
                         </div>
                     </div>
                 </div>
@@ -35,7 +35,8 @@
                         </div>
                     </div>
                 </div>
-                <div class="col-md-6">                  
+                <div class="col-md-6">
+                  {{-- span de tipo button que ejecuta funcion que trae datos del huesped --}}
                   <span class="btn btn-success pull-right" onclick="buscarHuesped()"><i class="fas fa-search"></i> Buscar</span>
                 </div>
                </div>        
@@ -43,16 +44,23 @@
               <div class="row">    
                   <div class="col-md-6">
                     <div class="input-group">
-                        <span class="input-group-addon">Reserva</span>                        
-                        <div class="form-group label-floating">                           
+                        <span class="input-group-addon">
+                            {{-- <i class="fas fa-restroom"></i> --}}Reserva
+                        </span>
+                        
+                        <div class="form-group label-floating">
+                            {{-- <label class="control-label">Reserva</label> --}}
                             <input type="text" class="form-control" id="reserva" name="reserva" readonly>  
                         </div>
                     </div>
                   </div>
                   <div class="col-md-6">
                       <div class="input-group">
-                        <span class="input-group-addon">Nombre</span>
-                        <div class="form-group label-floating">                            
+                        <span class="input-group-addon">
+                            {{-- <i class="fas fa-file-signature"></i> --}}Nombre
+                        </span>
+                        <div class="form-group label-floating">
+                            {{-- <label class="control-label">Nombre</label> --}}
                             <input type="text" class="form-control" id="nombre" name="nombre" readonly>  
                         </div>
                     </div>
@@ -61,16 +69,22 @@
               <div class="row">    
                   <div class="col-md-6">
                     <div class="input-group">
-                        <span class="input-group-addon">Room</span>
-                        <div class="form-group label-floating">                            
+                        <span class="input-group-addon">
+                            {{-- <i class="fas fa-child"></i> --}}room
+                        </span>
+                        <div class="form-group label-floating">
+                            {{-- <label class="control-label">Edad</label> --}}
                             <input type="text" class="form-control" id="room" name="room" readonly>  
                         </div>
                     </div>
                   </div>
                   <div class="col-md-6">
                       <div class="input-group">
-                        <span class="input-group-addon">Ocupante</span>
-                        <div class="form-group label-floating">                        
+                        <span class="input-group-addon">
+                            {{-- <i class="fas fa-users"></i> --}}Ocupante
+                        </span>
+                        <div class="form-group label-floating">
+                            {{-- <label class="control-label">Ocupante</label> --}}
                             <input type="number" class="form-control" id="ocupante" name="ocupante" >  
                         </div>
                     </div>
@@ -79,17 +93,22 @@
               <div class="row">    
                   <div class="col-md-6">
                     <div class="input-group">
-                        <span class="input-group-addon">Fecha salida</span>
-                        <div class="form-group label-floating">                            
+                        <span class="input-group-addon">
+                            {{-- <i class="fas fa-calendar-alt"></i> --}}Fecha salida
+                        </span>
+                        <div class="form-group label-floating">
+                            {{-- <label class="control-label">Fecha salida</label> --}}
                             <input type="text" class="form-control" id="fechaSalida" name="fechaSalida"  readonly>  
                         </div>
                     </div>
                   </div>
                   <div class="col-md-6">
                       <div class="input-group">
-                        <span class="input-group-addon">Brazalete
+                        <span class="input-group-addon">
+                            {{-- <i class="fas fa-ring"></i> --}}Brazalete
                         </span>
-                        <div class="form-group label-floating">                            
+                        <div class="form-group label-floating">
+                            {{-- <label class="control-label">Brazalete</label> --}}
                             <input type="text" class="form-control" id="brazalete" name="brazalete" readonly>  
                         </div>
                     </div>
@@ -116,7 +135,8 @@
       </div>
       <div class="modal-footer">
         <button type="button" class="btn btn-warning pull-left" data-dismiss="modal"> <i class="fas fa-undo"></i> Descartar</button>
-        <button type="button" class="btn btn-primary" onclick="abrirCuenta()"><i class="fas fa-sign-in-alt" ></i> Abrir mesa</button>        
+        <button type="button" class="btn btn-primary"><i class="fas fa-sign-in-alt" ></i> Abrir mesa</button>
+        
       </div>
     </div>
   </div>
